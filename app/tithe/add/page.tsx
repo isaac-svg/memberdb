716371ac7bed4308caae2b8a003095e702858888
isaac-svg <isaacsakyi009@gmail.com>
2024-09-29 12:31:39 +0000

    add tithe route and page skeletons

---
+++ app/tithe/add/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const AddTithe = (props: Props) => {
+  return <div>AddTithe</div>;
+};
+
+export default AddTithe;
---
+++ app/tithe/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const TithePage = (props: Props) => {
+  return <div>TithePage</div>;
+};
+
+export default TithePage;
